7f5039a2eac9a9817f7fb4ee756b4d9b33300228
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-06-03 11:15:49 -0500

    Started Splash Page

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -23,10 +23,10 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
         return;
     }
-    
+
     dispatch(setUser(data))
   }
-  
+
   export const login = (email, password) => async (dispatch)  => {
     const response = await fetch('/api/auth/login', {
       method: 'POST',
@@ -42,30 +42,33 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
         return data;
     }
-    
+
     dispatch(setUser(data))
     return {};
   }
-  
+
   export const logout = () => async (dispatch) => {
     const response = await fetch("/api/auth/logout", {
       headers: {
         "Content-Type": "application/json",
       }
     });
-    
+
     const data = await response.json();
     dispatch(removeUser());
   };
-  
-  
-  export const signUp = (username, email, password) => async (dispatch)  => {
+
+
+  export const signUp = (username, email, password, f_name, l_name, profile_pic) => async (dispatch)  => {
     const response = await fetch("/api/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        f_name,
+        l_name,
+        profile_pic,
         username,
         email,
         password,
@@ -75,7 +78,7 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
         return data;
     }
-    
+
     dispatch(setUser(data))
     return {};
   }
