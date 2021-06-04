81a7220572ecd6a18363f72f9118ea06d9066e26
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-06-03 23:45:03 -0500

    Worked on auth

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -5,14 +5,14 @@ import SignUpForm from './auth/SignUpForm';
 
 function SplashPage() {
     return (
-        <div className='bg'>
-            <div className="header">
-                <h1>Welcome to second degree!</h1>
-                <h2>The dating app for friends of friends</h2>
-                <LoginForm />
-                <SignUpForm />
+        <>
+            <div className='bg'>
+                <div className="header">
+                    <h1>Welcome to second degree!</h1>
+                    <h2>The dating app for friends of friends</h2>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -30,7 +30,7 @@ export const authenticate = () => async (dispatch) => {
   }
 
   export const login = (email, password) => async (dispatch)  => {
-    const response = await fetch('/api/auth/login', {
+    const response = await fetch('/api/auth/login/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -61,7 +61,7 @@ export const authenticate = () => async (dispatch) => {
   };
 
 
-  export const signUp = ( username, email, password, f_name) => async (dispatch)  => {
+  export const signUp = ( username, email, password, f_name, l_name, profile_pic) => async (dispatch)  => {
     console.log(username, email, f_name, password)
     const response = await fetch("/api/auth/signup", {
       method: "POST",
