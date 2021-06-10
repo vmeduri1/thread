0d2092663378205fbc92ae97df00401513a9b970
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-06-10 12:49:11 -0500

    Finished matches

--- app/seeds/matches.py
+++ app/seeds/matches.py
@@ -8,22 +8,24 @@ def seed_matches():
     # steveJ = User.query.filter_by(username="Jobs")
 
     demo_mark = Match(match_a=1, match_b=2)
-    mark_demo = Match(match_a=2, match_b=1)
     db.session.add(demo_mark)
+
+    mark_demo = Match(match_a=2, match_b=1)
     db.session.add(mark_demo)
 
     demo_hieu = Match(match_a=1, match_b=3)
-
     db.session.add(demo_hieu)
 
     demo_jobs = Match(match_a=1, match_b=4)
-
     db.session.add(demo_jobs)
 
     demo_whitney = Match(match_a=1, match_b=5)
     # whitney_demo = Match(match_a=5, match_b=1)
     # db.session.add(whitney_demo)
     db.session.add(demo_whitney)
+
+    elon_b = Match(match_a=12, match_b=13)
+    db.session.add(elon_b)
 
     db.session.commit()
 
