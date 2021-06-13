3ba05810e37fdea413f1ee0ee6a682d65c814249
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-06-13 14:30:18 -0500

    Added phone numbers, styled Edit and Logout Profile Btns

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/session";
 import { Redirect } from "react-router-dom";
+import './LogoutButton.css'
 
 
 
@@ -14,7 +15,7 @@ const LogoutButton = () => {
   if (!user) {
     return <Redirect to="/" />;
   }
-  return <button onClick={onLogout}>Logout</button>;
+  return <button className='logout-button' onClick={onLogout}>Logout</button>;
 };
 
 export default LogoutButton;
